6a77e904176d140acac155fcd777d150bd8f83bd
florimondmanca <florimond.manca@gmail.com>
2020-07-05 22:46:28 +0200

    Fix Scope type hint

--- src/asgi_sitemaps/_types.py
+++ src/asgi_sitemaps/_types.py
@@ -1,5 +1,5 @@
-from typing import AsyncIterable, Dict, Iterable, TypeVar, Union
+from typing import Any, AsyncIterable, Dict, Iterable, TypeVar, Union
 
 T = TypeVar("T")
 ItemsTypes = Union[Iterable[T], AsyncIterable[T]]
-Scope = Dict[str, str]
+Scope = Dict[str, Any]
